2aaaf983156c3e740c26fdac67007ab861676cb6
HAMANO Tsukasa <hamano@osstech.co.jp>
2021-02-10 22:56:09 +0900

    ITS#9463 cumulative fix for back-wt
    - LDAP MODRDN handling
    - support paged response
    - add wt_extended
    - add config emit
    - wt_key_read() return WT_NOTFOUND if not found key.
    - add ext_candidates()
    - fix idlcache session name
    - fix warning
    - don't reuse idlcache cursor
    - set correct pid when modrdn with newsuperior
    - fix condition bug
    - fix send_search_entry() error handling
    - fix for referral handling
    - fix for readonly mode
    - fix sizelimit response
    - support modrdn
    - improve modify handling
    - clear ancestor idlcache
    - fix for multi-DIT
    - IMPORTANT CHANGES: Compatibility is broken with previous database table, please restore database from LDIF.
    - checking for scope=children
    - sort dn2idl result
    - fix cursor leak
    - support db_open with readonly mode
    - add wt_tool_entry_delete
    - initialize comp variable
    - support referrals
    - implement wt_tool_dn2id_get() and wt_tool_entry_modify() for slapadd -w
    - skip redundant scan, and more debug message
    - fix OID conflict with back-passwd
    - no need to close session, It may cause SEGV.
    - fixed wt_dn2entry for empty DN
    - support multiple database
    - Construct wiredtiger's config parameter. It allow multi line wtconfig settings
    - add idlcache
    - fix concurrent modification to a entry with multi values
    - prevent to add duplicate dn entry
    - suppress error message "search_near failed: WT_NOTFOUND"
    - update Debug statements
    - back-wt does not support subtree rename
    - fix for @ondra review
    - update slapd-wt.5 and warning for mode option
    - add back-wt test into test target
    - add scope checking

--- servers/slapd/back-wt/idl.c
+++ servers/slapd/back-wt/idl.c
@@ -31,8 +31,7 @@
 #define IDL_MIN(x,y)	( (x) < (y) ? (x) : (y) )
 #define IDL_CMP(x,y)	( (x) < (y) ? -1 : (x) > (y) )
 
-#if IDL_DEBUG > 0
-static void idl_check( ID *ids )
+void wt_idl_check( ID *ids )
 {
 	if( WT_IDL_IS_RANGE( ids ) ) {
 		assert( WT_IDL_RANGE_FIRST(ids) <= WT_IDL_RANGE_LAST(ids) );
@@ -44,8 +43,7 @@ static void idl_check( ID *ids )
 	}
 }
 
-#if IDL_DEBUG > 1
-static void idl_dump( ID *ids )
+void wt_idl_dump( ID *ids )
 {
 	if( WT_IDL_IS_RANGE( ids ) ) {
 		Debug( LDAP_DEBUG_ANY,
@@ -67,10 +65,8 @@ static void idl_dump( ID *ids )
 		Debug( LDAP_DEBUG_ANY, "\n" );
 	}
 
-	idl_check( ids );
+	wt_idl_check( ids );
 }
-#endif /* IDL_DEBUG > 1 */
-#endif /* IDL_DEBUG > 0 */
 
 unsigned wt_idl_search( ID *ids, ID id )
 {
@@ -138,7 +134,7 @@ int wt_idl_insert( ID *ids, ID id )
 	Debug( LDAP_DEBUG_ANY, "insert: %04lx at %d\n", (long) id, x );
 	idl_dump( ids );
 #elif IDL_DEBUG > 0
-	idl_check( ids );
+	wt_idl_check( ids );
 #endif
 
 	if (WT_IDL_IS_RANGE( ids )) {
@@ -183,9 +179,9 @@ int wt_idl_insert( ID *ids, ID id )
 	}
 
 #if IDL_DEBUG > 1
-	idl_dump( ids );
+	wt_idl_dump( ids );
 #elif IDL_DEBUG > 0
-	idl_check( ids );
+	wt_idl_check( ids );
 #endif
 
 	return 0;
@@ -199,7 +195,7 @@ static int wt_idl_delete( ID *ids, ID id )
 	Debug( LDAP_DEBUG_ANY, "delete: %04lx at %d\n", (long) id, x );
 	idl_dump( ids );
 #elif IDL_DEBUG > 0
-	idl_check( ids );
+	wt_idl_check( ids );
 #endif
 
 	if (WT_IDL_IS_RANGE( ids )) {
@@ -240,9 +236,9 @@ static int wt_idl_delete( ID *ids, ID id )
 	}
 
 #if IDL_DEBUG > 1
-	idl_dump( ids );
+	wt_idl_dump( ids );
 #elif IDL_DEBUG > 0
-	idl_check( ids );
+	wt_idl_check( ids );
 #endif
 
 	return 0;
