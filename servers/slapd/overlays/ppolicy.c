f12d6c047c31c7b0009f10e3be05ae066bac61ac
HAMANO Tsukasa <code@cuspy.org>
2023-03-24 11:24:15 +0900

    ITS#10028 - crash with pwdMinDelay

--- servers/slapd/overlays/ppolicy.c
+++ servers/slapd/overlays/ppolicy.c
@@ -1687,7 +1687,8 @@ ppolicy_bind_response( Operation *op, SlapReply *rs )
 		} else if ( ppb->pp.pwdMinDelay ) {
 			int waittime = ppb->pp.pwdMinDelay << fc;
 			time_t wait_end;
-			struct berval lockout_stamp;
+			char lockout_stamp_buf[ LDAP_LUTIL_GENTIME_BUFSIZE ];
+			struct berval lockout_stamp = BER_BVC(lockout_stamp_buf);
 
 			if ( waittime > ppb->pp.pwdMaxDelay ) {
 				waittime = ppb->pp.pwdMaxDelay;
