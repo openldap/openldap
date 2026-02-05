c2fd8cc9cffd37a526a62a3160a5ef9708015ace
Ondřej Kuzník <ondra@mistotebe.net>
2026-02-05 16:16:17 +0000

    ITS#10440 slapo-syncprov: do not propagate request controls to internal ops

--- servers/slapd/overlays/syncprov.c
+++ servers/slapd/overlays/syncprov.c
@@ -2290,6 +2290,9 @@ syncprov_play_accesslog( Operation *op, SlapReply *rs, sync_control *srs,
 	fop.o_bd = db;
 	fop.o_dn = db->be_rootdn;
 	fop.o_ndn = db->be_rootndn;
+
+	memset( fop.o_ctrlflag, 0, sizeof(fop.o_ctrlflag) );
+
 	rc = be_entry_get_rw( &fop, &si->si_logbase, NULL, ad_minCSN, 0, &e );
 	if ( rc ) {
 		return rc;
