066f04d62dfe0939917dc0c30100d1b08fff3ba8
Ondřej Kuzník <ondra@mistotebe.net>
2026-03-24 15:16:42 +0000

    ITS#10471 Silence a warning

--- servers/lloadd/daemon.c
+++ servers/lloadd/daemon.c
@@ -189,6 +189,7 @@ get_url_perms( char **exts, mode_t *perms )
                     /* skip leading '0' */
                     if ( value[0] != '0' ) return LDAP_OTHER;
                     value++;
+                    /* fallthru */
 
                 case 3:
                     for ( j = 0; j < 3; j++ ) {
