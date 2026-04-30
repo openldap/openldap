9cbcfe748a07c707795bd331a1db67000ae7b302
Ondřej Kuzník <ondra@mistotebe.net>
2026-04-30 15:34:26 +0100

    ITS#10499 tests: record test results as junit.xml

--- tests/scripts/functions.sh
+++ tests/scripts/functions.sh
@@ -20,3 +20,37 @@ timer() {
 		date -u $DATEOPT$delta +%T
 	fi
 }
+
+xml_escape() {
+	sed -e 's/&/\&amp;/g' -e 's/</\&lt;/g' -e 's/>/\&gt;/g'
+}
+
+junit_finalize() {
+	[ -n "$JUNIT_OUTPUT" ] || return 0
+	[ -f "$JUNIT_TMP" ] || return 0
+	JUNIT_END=`date +%s`
+	JUNIT_TOTAL=`grep -c '<testcase' "$JUNIT_TMP" 2>/dev/null`
+	[ -z "$JUNIT_TOTAL" ] && JUNIT_TOTAL=0
+	{
+		echo '<?xml version="1.0" encoding="UTF-8"?>'
+		printf '<testsuite name="%s" tests="%d" failures="%d" skipped="%d" time="%d">\n' \
+			"$JUNIT_NAME" "$JUNIT_TOTAL" "$FAILCOUNT" "$SKIPCOUNT" \
+			"$(( $JUNIT_END - $JUNIT_START ))"
+		cat "$JUNIT_TMP"
+		echo '</testsuite>'
+	} > "$JUNIT_OUTPUT"
+	rm -f "$JUNIT_TMP" "$JUNIT_LOG" "$JUNIT_RC"
+}
+
+junit_setup() {
+	JUNIT_NAME="${1:-$BACKEND}"
+	JUNIT_OUTPUT="${JUNIT_OUTPUT-$TESTWD/junit$TESTINST-$JUNIT_NAME.xml}"
+	if [ -n "$JUNIT_OUTPUT" ]; then
+		JUNIT_TMP="$TESTWD/junit_tmp$TESTINST"
+		JUNIT_LOG="$TESTWD/junit_current$TESTINST.log"
+		JUNIT_RC="$TESTWD/junit_current$TESTINST.rc"
+		: > "$JUNIT_TMP"
+		JUNIT_START=`date +%s`
+		trap junit_finalize EXIT
+	fi
+}
