b09d4347c4578586221e8a671882f175eddc62b5
Howard Chu <hyc@openldap.org>
2026-07-20 17:42:05 +0100

    ITS#10541 syncrepl: assert on entryCSN must use normalized value

--- servers/slapd/syncrepl.c
+++ servers/slapd/syncrepl.c
@@ -5819,8 +5819,8 @@ dn_callback(
 									old->a_vals[0].bv_val );
 								return LDAP_SUCCESS;
 							}
-							memcpy( dni->oldcsn, old->a_vals[0].bv_val,
-									old->a_vals[0].bv_len+1 );
+							memcpy( dni->oldcsn, old->a_nvals[0].bv_val,
+									old->a_nvals[0].bv_len+1 );
 						}
 					}
 
