6f0a32496a5aadee15a5e5103c479bd3355ae273
Quanah Gibson-Mount <quanah@openldap.org>
2026-08-06 17:11:09 +0000

    Prep for release (LMDB 1.0.1)

--- libraries/liblmdb/lmdb.h
+++ libraries/liblmdb/lmdb.h
@@ -236,7 +236,7 @@ typedef int mdb_filehandle_t;
 /** Library minor version */
 #define MDB_VERSION_MINOR	0
 /** Library patch version */
-#define MDB_VERSION_PATCH	0
+#define MDB_VERSION_PATCH	1
 
 /** Combine args a,b,c into a single integer for easy version comparisons */
 #define MDB_VERINT(a,b,c)	(((a) << 24) | ((b) << 16) | (c))
@@ -246,7 +246,7 @@ typedef int mdb_filehandle_t;
 	MDB_VERINT(MDB_VERSION_MAJOR,MDB_VERSION_MINOR,MDB_VERSION_PATCH)
 
 /** The release date of this library version */
-#define MDB_VERSION_DATE	"June 30, 2026"
+#define MDB_VERSION_DATE	"Aug 6, 2026"
 
 /** A stringifier for the version info */
 #define MDB_VERSTR(a,b,c,d)	"LMDB " #a "." #b "." #c ": (" d ")"
